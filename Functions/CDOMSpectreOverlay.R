8c2ddaec4df7ee68f1b56012c235a0a47083fb92
RichardLaBrie <richard.labrie90@gmail.com>
2017-04-27 15:26:11 -0400

    Harmonized all script writing

--- Functions/CDOMSpectreOverlay.R
+++ Functions/CDOMSpectreOverlay.R
@@ -6,22 +6,22 @@ CDOMOverlay <- function()
 	CDOM = list()
 	for(i in 1:length(file.data))
 	{
-		data = read.table(paste(".//CDOM//",file.data[i],sep=""),skip=1,header=1,sep=",")
-		WV <- data[,1]
-		abs <- data[,2]
-		CDOM[[i]] = cbind(WV,abs)
+		data = read.table(paste(".//CDOM//", file.data[i], sep=""), skip = 1, header = 1, sep=",")
+		WV = data[,1]
+		abs = data[,2]
+		CDOM[[i]] = cbind(WV, abs)
 	}
 	pdf("CDOM_courbes.pdf")
 	for(i in 1:length(CDOM))
 	{
     CDOM.temp = CDOM[[i]][,2]
-		if(i==1)
+		if(i == 1)
 		{
-			plot(CDOM.temp~CDOM[[i]][,1],xlim=c(190,900),ylim=c(0,3),type="l",main="")
+			plot(CDOM.temp ~ CDOM[[i]][,1], xlim = c(190, 900), ylim = c(0, 3), type = "l", main = "")
 		}
 		else
 		{
-			lines(CDOM.temp~CDOM[[i]][,1])
+			lines(CDOM.temp ~ CDOM[[i]][,1])
 		}
 	}
   dev.off()
--- Functions/InnerFilter.R
+++ Functions/InnerFilter.R
@@ -1,7 +1,7 @@
-InnerFilter = function(path, cube, excitation = c(220,450,5), emission = c(230, 600, 2),pathlength=1,name.ref)
+InnerFilter = function(path, cube, excitation = c(220,450,5), emission = c(230, 600, 2), pathlength=1, name.ref, skip)
 {
-	wlex <- seq(excitation[1], excitation[2], excitation[3])
-	wlem <- seq(emission[1], emission[2], emission[3])
+	wlex = seq(excitation[1], excitation[2], excitation[3])
+	wlem = seq(emission[1], emission[2], emission[3])
   filename = unlist(name.ref)
 	
   
@@ -19,30 +19,30 @@ InnerFilter = function(path, cube, excitation = c(220,450,5), emission = c(230, 
 	
 	file.data = unlist(file.list)
   file.create("..\\verifyMatches.csv")
-  write.table(t(c("Target","Source","Distance")),"..\\verifyMatches.csv",append=T,sep=",",col.names = F)
+  write.table(t(c("Target", "Source", "Distance")), "..\\verifyMatches.csv", append=T, sep=",", col.names = F)
   
-	selectMinStringDist <- function(target,source)
+	selectMinStringDist = function(target, source)
 	{
-  	dist=stringdist(target,source,method="lcs")
-	  index=which.min(dist)
-	  min=dist[index]
+  	dist = stringdist(target, source, method="lcs")
+	  index = which.min(dist)
+	  min = dist[index]
 	  #if((dist[index]-12)>0)warning(paste("Dist was",dist[index]-12,"for",target,source[index]))
-	  write.table(t(c(target,source[index],min)),"..\\verifyMatches.csv",append=T,sep=",",col.names = F)
+	  write.table(t(c(target, source[index] ,min)), "..\\verifyMatches.csv", append=T, sep = ",", col.names = F)
 	  return(c(index))
 	}
 
-	index=sapply(filename,	selectMinStringDist,source=file.data)
-  file.data=file.data[index]
+	index = sapply(filename, selectMinStringDist, source = file.data)
+  file.data = file.data[index]
 	
 	for(i in 1:length(file.data))
 	{
-		Abs = read.table(file.data[i],skip=1,header=2,sep=",")
-		WV <- Abs[,1]
-		Absex = sapply(wlex,function(x){return(subset(Abs[,2],WV==x))})
-		Absem = sapply(wlem,function(x){return(subset(Abs[,2],WV==x))})
+		Abs = read.table(file.data[i], skip = skip, header = skip + 1, sep=",")
+		WV = Abs[,1]
+		Absex = sapply(wlex, function(x){return(subset(Abs[,2], WV == x))})
+		Absem = sapply(wlem, function(x){return(subset(Abs[,2], WV == x))})
 		Amat = outer(Absex,Absem,'+')
-		Amat2 = 10^(-0.5*pathlength*Amat)
-		cube[,,i] = cube[,,i]/Amat2
+		Amat2 = 10^(-0.5 * pathlength * Amat)
+		cube[,,i] = cube[,,i] / Amat2
 	}
 return(cube)
 }
