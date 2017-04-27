971e875a4b89c116e4626f1cacfb3f6c7bd86f00
RichardLaBrie <richard.labrie90@gmail.com>
2017-04-27 14:24:51 -0400

    Possibility to plot in R added

--- Functions/PlotAllEEM.R
+++ Functions/PlotAllEEM.R
@@ -1,12 +1,18 @@
-PlotAllEEM <- function(cube,zlim=c(0,1))
+PlotAllEEM <- function(cube,zlim=c(0,1),ExportPDF=T)
 {
 	setwd(getwd())
-	pdf("EEM.pdf")
-	for(i in 1:cube[[5]])
+	if(ExportPDF)
+	{
+	  pdf("EEM.pdf")
+	}
+	  for(i in 1:cube[[5]])
 	{
 		filled.contour(cube[[3]], cube[[4]], unlist(cube[[1]])[,,i],color.palette=myPalette,
 		xlab="Excitation (nm)", ylab="Emission (nm)",
 		main=unlist(cube[[2]])[i], zlim = zlim, nlevels=50)
 	}
-	dev.off()
+	if(ExportPDF)
+	{
+    dev.off()
+	}
  }
