e594a7cb99d3a1f5d5e03d21157e88331df6b60a
Richard_LaBrie <richard.labrie90@gmail.com>
2017-06-29 10:17:31 -0400

    :wrench: Functions now work adequately

--- Functions/CDOMIndices.R
+++ Functions/CDOMIndices.R
@@ -1,43 +1,96 @@
 #Wavelengths chosen based on Helms 2008
-#Calculate spectral slope of wavelength (1) with reference wavelength (0)
-#The order of the argument is CDOM file, wavelength (1), reference wavelength (0)
+#Calculate spectral slope with a reference wavelength
+#FileSelect is a logical argument for the number of files to load. Default is FALSE, meaning that the function
+  #will automatically load and calculate S for all CDOM files "./data/CDOM"
+  #If TRUE, you get to choose the files you want
+#wl0 is the reference wavelength, default is 375 as in Stedmon, Markager and Kaas 2000
+#From is the first wavelength needed to compute the exponential fit, default is 300 as in Stedmon, Markager and Kaas 2000
+#To is the last wavelength needed, default is 650 as in Stedmon, Markager and Kaas 2000
+#By is the step between each wavelength, default is 1
+#skip is the number of lines to skip before getting the header in CDOM files
 
-SpectralSlope <- function(CDOM, wl1, wl0)
+#The model use a nonlinear least square fitting routine
+#The equation is abs = abs0 * exp(-S(wavelength - ref.wavelength)) + K as in Markager & Vincent 2000
+#where abs is the absorption coef, abs0 is the reference absorption coef at wavelenth and ref.wavelength
+#S is the slope and K is an additional background parameter to allow baseline to shift 
+
+
+SpectralSlope <- function(FileSelect = F, wl0 = 375, From = 300, To = 650, By = 1, skip = 1)
 {
-  #Find absorption coefficient at relevant wavelength
-  a0 = CDOM[,2][CDOM[,1] == wl0]
-  a1 = CDOM[,2][CDOM[,1] == wl1]
-  
-  #Calculate log S
-  Slog = -log(a0) / ((wl0 - wl1) * log(a1))
-  
-  #Back calculate S to use it in Sr
-  S = exp(Slog)
-  return(S)
+    setwd("./data/CDOM")
+    if(!FileSelect) file.dir = list.files()
+    if(FileSelect) file.dir = choose.files()
+    #create sequence of desired wavelengths
+    wl.x = seq(From, To, By)
+    #Create the output matrix
+    output = matrix(0,ncol = 4, nrow = length(file.dir))
+    colnames(output) = c("Intercept", "Slope", "R2", "K")
+    rownames(output) = file.dir
+    
+    #Compute the exponential fit for each CDOM file
+    for(i in 1:length(file.dir))
+    {
+      Abs = read.table(file.dir[i], skip = skip, header = skip + 1, sep=",")
+      WL = Abs[,1]
+      a0 = Abs[which(Abs[,1] == wl0),2]
+      Abs.y = sapply(wl.x, function(x){return(subset(Abs[,2], WL == x))})
+      nls.temp <- nls(Abs.y ~ a0 * exp(-S * (wl.x - wl0)) + K, 
+                  start = list(a0 = a0, S = 0.02, K = 0.02), #a0 is bounded between 0 and the highest observed value
+                  lower = list(a0 = 0, S = 0, K = 0), #S is bounded between 0 and 1 otherwise the function grows
+                  upper = list(a0 = max(Abs.y), S = 1, K = 3),
+                  control = list(maxiter = 200, warnOnly = T),
+                  algorithm = "port") #K is theoritically unbound [-Inf:Inf], but such values make no sense in this context. We've set the boundary to 0 and 3, which is the limit for absorption correction used with EEMs ()
+      
+      R2 <- 1 - sum((Abs.y - predict(nls.temp))^2) / (length(Abs.y) * var(Abs.y)) #Denominator is sum(y-mean(y))² which is variance(y) times length(y)
+      
+      output[i,1] = coef(nls.temp)[3]
+      output[i,2] = coef(nls.temp)[2]
+      output[i,3] = R2
+      output[i,4] = coef(nls.temp)[3]
+    }
+    
+    #Go back to main folder
+    setwd("..")
+    setwd("..")
+    #Return the result
+    return(output)
 }
 
+#FileSelect is a logical argument for the number of files to load. Default is FALSE, meaning that the function
+  #will automatically load and calculate S for all CDOM files "./data/CDOM"
+  #If TRUE, you get to choose the files you want
 #wl1.1 is the desired wavelength for the spectral ratio numerator
 #wl1.0 is the reference wavelength for the spectral ratio numerator
 #wl2.1 is the desired wavelength for the spectral ratio denominator
 #wl2.0 is the reference wavelength for the spectral ratio denominator
+#By is the step between each wavelength, default is 1
 #skip is the number of lines to skip before getting the header in CDOM files
 
-Sr <- function(wl1.1 = 275, wl1.0 = 295, wl2.1 = 350, wl2.0 = 400, skip = 1)
+Sr <- function(FileSelect = F, wl1.1 = 275, wl1.2 = 295, wl2.1 = 350, wl2.2 = 400, By = 1, skip = 1)
 {
   setwd("./data/CDOM")
-  file.dir = list.files()
-  
-  Sr = 0
+  if(!FileSelect) file.dir = list.files()
+  if(FileSelect) file.dir = choose.files()
+  wl.num = seq(wl1.1, wl1.2, By)
+  wl.denom = seq(wl2.1, wl2.2, By)
+  Sr = matrix(0, nrow = length(file.dir), ncol=1)
+  rownames(Sr) = file.dir
   
   for(i in 1:length(file.dir)) 
   {
     CDOM = read.table(file.dir[i], skip = skip, header = skip + 1, sep=",")
+    WL = CDOM[,1]
+    
+    Abs.num = sapply(wl.num, function(x){return(subset(CDOM[,2], WL == x))})
+    Abs.denom = sapply(wl.denom, function(x){return(subset(CDOM[,2], WL == x))})
+    
+    
     #Calculate S for wl = 275 and wl = 350, based on Helms 2008
-    S275 = SpectralSlope(CDOM, wl1.1, wl1.0)
-    S350 = SpectralSlope(CDOM, wl2.1, wl2.0)
+    Slope.num = coef(lm(log(Abs.num) ~ wl.num))[2]
+    SLope.denom = coef(lm(log(Abs.denom) ~ wl.denom))[2]
     
     #Calculate the slope ratio
-    Sr[i] = S275 / S350
+    Sr[i,1] = Slope.num / SLope.denom
   }
 
   #Go back to main folder
@@ -56,11 +109,12 @@ Sr <- function(wl1.1 = 275, wl1.0 = 295, wl2.1 = 350, wl2.0 = 400, skip = 1)
 #wl is set at 254 for SUVA254 but can be change to obtain any desired wavelength
 #skip is the number of lines to skip before getting the header in CDOM files
 
-SUVA <- function(DOC, wl = 254, unit = "mg/L", skip = 1, name="SUVAMatches")
+SUVA <- function(FileSelect = F,DOC, wl = 254, unit = "mg/L", skip = 1, name="SUVAMatches")
 {
   if(unit == "uM") DOC = DOC * 12 / 1000
   setwd("./data/CDOM")
-  file.dir = list.files()
+  if(!FileSelect) file.dir = list.files()
+  if(FileSelect) file.dir = choose.files()
   
   #Create the .csv file to make sure matches were done correctly
   file.create("..\\SUVAMatches.csv")
@@ -71,19 +125,17 @@ SUVA <- function(DOC, wl = 254, unit = "mg/L", skip = 1, name="SUVAMatches")
   file.dir = file.dir[index]
   
   #Calculate SUVA at wavelength = wl
-  SUVA254 = list()
+  SUVA254 = matrix(0, nrow = length(file.dir))
   for(i in 1:length(file.dir)) 
   {
     Abs = read.table(file.dir[i], skip = skip, header = skip + 1, sep=",")
     a254 = Abs[,2][Abs[,1] == wl]
-    SUVA254[i] = a254 / DOC[i]
+    SUVA254[i,1] = a254 / DOC[i]
   }
   
   #Return to main folder
   setwd("..")
   setwd("..")
-  #Create a matrix with the values
-  SUVA254 = as.matrix(unlist(SUVA254))
   #Name column and rows
   rownames(SUVA254) = rownames(DOC)
   colnames(SUVA254) = "SUVA"
