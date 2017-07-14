c034ea5334dd0b467dea31185975a90bb60af964
RichardLaBrie <richard.labrie90@gmail.com>
2017-07-14 16:52:08 -0400

    Update FolderCreation.R

--- R/FolderCreation.R
+++ R/FolderCreation.R
@@ -1,3 +1,12 @@
+#'@title Creates data folder, subfolder and R project
+
+#'@description This function creates folders and .Rproj file require by the various functions to work.
+#'These steps can be done by hand instead.
+
+#'@param path.to.folder is the path where the working directory is
+#'@param WorkingDirectory is the name of the working directory
+#'@param subfiles is the list of folder where to put cdom, fdom and nano water samples
+
 #'@export
 
 #Example: FolderCreation(path.to.folder = "D:/test", WorkingDirectory = "test")
