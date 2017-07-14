b6ab190fe8b57b1e689524876959e56f3e20d6d2
RichardLaBrie <richard.labrie90@gmail.com>
2017-07-14 16:56:42 -0400

    Update PlotAllEEM.R

--- R/PlotAllEEM.R
+++ R/PlotAllEEM.R
@@ -1,3 +1,12 @@
+#'@title This function plot all corrected EEMs
+#'
+#'@description This function uses the output of PARAFAC.cube.design to plot all corrected EEMss at once
+#'The scale bar is the same among all samples. It also generates a pdf file containing all plots
+
+#'@param cube is the ouput of PARAFAC.cube.design
+#'@param zlim is the intensity to plot the EEMs. Negative and values outside of scale bar will be white
+#'@param ExportPDF is a logical paramater to export plots in a pdf file. Default is TRUE
+
 #'@export
 
 PlotAllEEM <- function(cube, zlim = c(0,1), ExportPDF = T)
