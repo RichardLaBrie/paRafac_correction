6093f661e9e4cdc5149284a61c62b3be655ec156
Richard_LaBrie <richard.labrie90@gmail.com>
2017-07-14 12:40:21 -0400

    :heavy_plus_sign: Function to create .Rproj file and data folders according to pathways require in the package

---
+++ R/FolderCreation.R
@@ -0,0 +1,15 @@
+#Example: FolderCreation(path.to.folder = "D:/test", WorkingDirectory = "test")
+
+FolderCreation <- function(path.to.folder,WorkingDirectory, subfiles = c("CDOM","FDOM","nano"))
+{
+  path <- file.path(path.to.folder, paste0(WorkingDirectory, ".Rproj"))
+  template_path <- system.file("templates/template.Rproj", package = "devtools")
+  file.copy(template_path, path)
+  
+  dir.create(file.path(path.to.folder,"data"), showWarnings = FALSE)
+  for(i in 1:3)
+  {
+    dir.create(file.path(path.to.folder,"/data",subfiles[i]), showWarnings = FALSE)
+  }
+  
+}
