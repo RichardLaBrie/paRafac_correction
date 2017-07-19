c4a7c34bc5a5222a097c67c94964ad167b98fe40
RichardLaBrie <richard.labrie90@gmail.com>
2017-07-19 16:16:43 -0400

    :wrench: Fixed bug

--- R/InnerFilter.R
+++ R/InnerFilter.R
@@ -30,7 +30,6 @@ InnerFilter = function(cube, excitation = c(220,450,5), emission = c(230, 600, 2
 	for(i in 1:length(file.dir)) #when file.dir, it was file.data
 	{
 		Abs = read.table(file.dir[i], skip = skip, header = skip + 1, sep=",")
-		WV = Abs[,1]
 		if((min(wlex) | min(wlem)) <= min(Abs[,1])) low = T
 		if((max(wlex) | max(wlem)) >= max(Abs[,1])) high = T
 		if(low | high)
@@ -41,6 +40,7 @@ InnerFilter = function(cube, excitation = c(220,450,5), emission = c(230, 600, 2
 			if(high) highto = max(max(wlem), max(wlex))
 			Abs = CDOMFileCorrection(CDOM = Abs, low = low, high = high, lowfrom, lowto, highfrom, highto)
 		}
+		WV = Abs[,1]
 		Absex = sapply(wlex, function(x){return(subset(Abs[,2], WV == x))})
 		Absem = sapply(wlem, function(x){return(subset(Abs[,2], WV == x))})
 		Amat = outer(Absex,Absem,'+')
