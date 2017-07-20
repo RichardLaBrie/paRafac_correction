2673a96bf5d3b7254f263c5c31e3239c1610d78b
Richard_LaBrie <richard.labrie90@gmail.com>
2017-07-20 16:19:26 -0400

    :wrench: added devtools library dependancy

--- R/SOURCEME.R
+++ R/SOURCEME.R
@@ -2,6 +2,8 @@ if(!require(stringr)) install.packages("stringr")
 library("stringr")
 if(!require(stringdist)) install.packages("stringdist")
 library("stringdist")
+if(!require(devtools)) install.packages("devtools")
+library("devtools")
 temp <- list.files(path = "./R")
 temp = temp[-grep("SOURCEME",temp)]
 for(i in 1:length(temp))
