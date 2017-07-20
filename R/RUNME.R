4ff6872e84b479569530f58c7d03b33f5d332928
Richard_LaBrie <richard.labrie90@gmail.com>
2017-07-20 16:15:29 -0400

    :heavy_plus_sign: New function for developers

---
+++ R/RUNME.R
@@ -0,0 +1,10 @@
+if(!require(stringr)) install.packages("stringr")
+library("stringr")
+if(!require(stringdist)) install.packages("stringdist")
+library("stringdist")
+temp <- list.files(path = "./R")
+temp = temp[-grep("RUNME",temp)]
+for(i in 1:length(temp))
+{
+  source(paste0("./R/",temp[i]))
+}
