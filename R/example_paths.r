2491df86d58c0c7ba108e49f918a5f077c97876e
nicolasfstgelais <nicolasfstgelais@gmail.com>
2017-07-21 11:06:37 -0400

    minor changes to example_paths.r

--- R/example_paths.r
+++ R/example_paths.r
@@ -1,2 +1,4 @@
-files=data.frame(paths=paste0("data/",list.files("data",recursive=T)))
-write.csv(files,"exemple_paths.csv")
+paths=as.character(paste0("data/",list.files("data",recursive=T)))
+files=data.frame(paths=c(paths,"Emcorr.csv","Excorr.csv"))
+write.csv(files,"example_paths.csv")
+
