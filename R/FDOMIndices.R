f07c0a27642c22c6d6bd43ad854237807f8bdd01
RichardLaBrie <richard.labrie90@gmail.com>
2018-04-12 14:53:19 -0400

    :wrench: Updated BIX

--- R/FDOMIndices.R
+++ R/FDOMIndices.R
@@ -68,7 +68,8 @@ BIX <- function(cube)
   if(!is.list(cube)) return(print("You need the complete list of PARAFAC.cube.design output"))
   
   em1 = 380
-  em2 = 430
+  em2 = 420
+  em3 = 436
   ex = 310
   
   output = matrix(0, nrow = cube[[5]])
@@ -77,8 +78,9 @@ BIX <- function(cube)
   for(k in 1:cube[[5]])
   {
     Fluo.em1 = cube[[1]][which(cube[[3]]==ex), which(cube[[4]]==em1), k]
-    Fluo.em2 = cube[[1]][which(cube[[3]]==ex), which(cube[[4]]==em2), k]
-    output[k,1] = Fluo.em1 / Fluo.em2
+    Fluo.em2 = cube[[1]][which(cube[[3]]==ex), c(which(cube[[4]]==em2):which(cube[[4]] == em3)), k]
+    Fluo.em2 = c(Fluo.em2, mean(c(Fluo.em2[length(Fluo.em2) - 1], Fluo.em2[length(Fluo.em2)])))
+    output[k,1] = Fluo.em1 / max(Fluo.em2)
   }
   
   return(output)
