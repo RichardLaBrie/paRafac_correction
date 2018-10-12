65cc101954e47b5a0360c1c4e97486132ed08b2c
RichardLaBrie <richard.labrie90@gmail.com>
2018-10-12 15:01:49 -0400

    Update CDOM340.R

--- R/CDOM340.R
+++ R/CDOM340.R
@@ -5,29 +5,29 @@
 #'@param FileSelect is a logical argument for the number of files to load. Default is FALSE, meaning that the function
 #will automatically load and calculate S for all CDOM files "./data/CDOM"
 #If TRUE, you get to choose the files you want
-#'@param nm1 is the desired wavelength. Default is 340
+#'@param nm1 is the desired wavelength. Default is 440
 #'@param skip is the number of lines to skip before getting the header in CDOM files
 
 
 #'@export
 
-Abs.val <- function(data.file = "data", FileSelect = F, skip = 1, nm = 340)
+Abs.val <- function(data.file = "data", FileSelect = F, skip = 1, nm = 440)
 {
 if(!FileSelect) file.dir = list.files("./data/CDOM")
 if(FileSelect) file.dir = choose.files("./data/CDOM")
 
-CDOM340 = matrix(0, nrow = length(file.dir), ncol=1)
-rownames(CDOM340) = file.dir
+CDOM440 = matrix(0, nrow = length(file.dir), ncol=1)
+rownames(CDOM440) = file.dir
 
 for(i in 1:length(file.dir)) 
 {
   CDOM = read.table(paste0("./",data.file,"/CDOM/",file.dir[i]), skip = skip, header = skip + 1, sep=",")
   WL = CDOM[,1]
   
-  CDOM340[i] = subset(CDOM[,2], WL == nm)
+  CDOM440[i] = subset(CDOM[,2], WL == nm)
 }
 
 #Return result
-return(CDOM340)
+return(CDOM440)
 
 }
